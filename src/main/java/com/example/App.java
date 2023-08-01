728b5d2aac0bf0201bef95f1a7ec431b67fe7031
SURU SURAJ <74001520+s-u-r-u@users.noreply.github.com>
2023-08-01 13:02:02 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,6 +4,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        private static final Logger logger = Logger.getLogger(App.class.getName());
         logger.info( "Hello World!" );
     }
 }
